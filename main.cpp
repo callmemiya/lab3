8643072e9bb54cb536170f21c77fbe4f771cacc9
callmemiya <56102704+callmemiya@users.noreply.github.com>
2020-03-30 12:18:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,33 +2,6 @@
 #include <fstream>
 #include <cstdlib>
 using namespace std;
-class Vector{
-protected:
-    int x,y;
-public:
-    Vector() {
-        x = 0;
-        y = 0;
-    }
-    Vector(int x,int y){
-        this->x=x;
-        this->y=y;
-    }
-    virtual void print(){
-        cout <<"\n" <<x <<";" <<y;
-    }
-};
-class Vector3 : public Vector {
-    int z;
-public:
-    Vector3(int x,int y,int z): Vector(x,y){
-        this->z=z;
-    }
-    void print(){
-        Vector::print();
-        cout <<";" <<z;
-    }
-};
 int main(){
     Vector* vector;
     vector = new Vector(1, 2);
